2587d69a931390941b57b5aba8d4dff7055316b8
shinthantequi@gmail.com <shinthantequi@gmail.com>
2022-06-08 20:13:01 +0630

    styling and finish with class component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,13 +1,16 @@
 import { Component } from "react";
-
+import Card from "./card.component";
+import './card-list.styles.css'
 class CardList extends Component{
   render(){
       const {monsters} = this.props;
       return(
-          <div className="card-container">
+          <div className="card-list">
               {
                  monsters.map((monster)=>{
-                     return <h1 key={monster.id}>{monster.name}</h1>
+                     return (
+                        <Card monster={monster} key={monster.id}/>
+                     )
                  })
               }
           </div>
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,11 +1,12 @@
 import { Component } from "react";
+import './search-box.styles.css';
 class SearchBox extends Component{
     render(){
         const {onChangeHandler}=this.props;
         return (
             <input
             type="search"
-            className={this.props.className}
+            className={`search-box ${this.props.className}`}
             placeholder={this.props.placeholder}
             onChange={onChangeHandler}
           />
